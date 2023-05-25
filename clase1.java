f2a7f496b03db65d1419666beb6c6f5abee88417
LichtEwig <lichtewig73@gmail.com>
2023-05-25 00:05:27 -0300

    Java

---
+++ clase1.java
@@ -0,0 +1,44 @@
+**
+ * 	Clase principal del proyecto!
+
+ */
+public class Hola {
+	/**
+	 * método principal del clase, punto de entrada!
+	 * @param args argumentos que ingresan de consola
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo!");
+
+		// en consola java -version verificamos la versión runtime
+		// javac -version verificamos la versión del compilador
+
+		// Comentarios de una sola sola linea
+		// El usuario final no puede ver este comentario
+
+		/*
+		 * Bloque
+		 * de
+		 * comentarios
+		 * El usuario final no puede ver este comentario
+		 */
+
+		// TODO tarea pendiente
+
+		/**
+		 * Comentario JAVA DOC.
+		 * El usuario final puede ver este comentario.
+		 * Este comentario, debe colocarse delante de la declaración
+		 * de método o clase.
+		 */
+
+		//Lenguajes de tipado fuerte:	C C++ C# java visualbasic typescript
+		//Lenguajes de tipado debil:	php javascript python
+
+
+		/*
+		 * 	Memoria RAM:		Volatil				Costo alto			Veloz
+		 * 
+		 * 	Disco Duro:			Persistente			Costo bajo			Lento
+		 * 
+		 */
